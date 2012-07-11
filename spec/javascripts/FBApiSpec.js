57154141db014b0932220115b076dc501da7c364
Ali Faiz <tech_ops+ali@change.org>
2012-07-11 13:48:12 -0700

    Add test for failed FB.api call

--- spec/javascripts/FBApiSpec.js
+++ spec/javascripts/FBApiSpec.js
@@ -124,6 +124,17 @@ describe("FB.api", function() {
       });
     });
 
+    describe("when I call FB.api(null, 'post', params, callback)", function() {
+      it("should callback with a fail message", function() {
+        FB.api(null, 'post', {}, function (r) {
+          expect(r.error).toBeDefined();
+          expect(r.error.code).toBeDefined();
+          expect(r.error.message).toBeDefined();
+          expect(r.error.type).toBeDefined();
+        });
+      });
+    });
+
   });
 
   describe("when not connected", function () {
